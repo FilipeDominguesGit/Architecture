f7af28b04ab1473e6f8c7955bcd5df5d9fdff078
Filipe Domingues <filipe.domingues.git@gmail.com>
2019-03-18 21:43:50 +0000

    added 1 example unit test

---
+++ onion/Tests/Core/Domain/Onion.Core.Domain.Tests/Services/OrderDomainServiceTests.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using Moq;
+using NUnit.Framework;
+using Onion.Core.Domain.Models;
+using Onion.Core.Domain.Repositories;
+using Onion.Core.Domain.Services;
+
+namespace Onion.Core.Domain.Tests.Services
+{
+    [TestFixture]
+    public class OrderDomainServiceTests
+    {
+
+        private IOrderDomainService _orderDomainService;
+        private Mock<IInventoryRepository> _inventoryRepository;
+        private Mock<IOrderRepository> _orderRepository;
+
+        [SetUp]
+        public void Setup()
+        {
+            _inventoryRepository = new Mock<IInventoryRepository>();
+            _orderRepository = new Mock<IOrderRepository>();
+        }
+
+        [Test]
+        public void AddProductToOrder_AddsProductToOrder_WhenHasAvailableOnInventory()
+        {
+            // arrange
+            _orderDomainService = new OrderDomainService(_inventoryRepository.Object,_orderRepository.Object);
+
+            var product = new Product()
+            {
+                Id = 1,
+                Brand = "BrandA",
+                Category = "CategoryA",
+                Name = "Prod Name",
+                Price = 100
+            };
+            var order = new Order(new User() {Id = 1, FirstName = "FirstName", LastName = "LastName"});
+
+            _inventoryRepository.Setup(r => r.GetById(It.IsAny<int>())).Returns(() => new Inventory()
+            {
+                Quantity = 1,
+                Product = new Product() { Id = 1 }
+            });
+
+
+            // act 
+            _orderDomainService.AddProductToOrder(product,order);
+
+            // assert
+            Assert.IsTrue(order.Products.Count == 1);
+            Assert.AreEqual(product,order.Products.First());
+
+        }
+
+
+    }
+}
